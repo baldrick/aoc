4f98268391b5cf8f3d34b2f385798e2d5b8a024b
Gavin Manning <git@GavinManning.com>
2024-12-14 21:31:27 +0000

    Improve template for test

--- 2024/14/a_test.go
+++ 2024/14/a_test.go
@@ -2,6 +2,7 @@ package day14
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/baldrick/aoc/common/aoc"
@@ -9,26 +10,25 @@ import (
 
 func Test_day14(t *testing.T) {
 	tests := []struct {
-		input        []string
+		input        string
 		wantA, wantB int
 		size         aoc.PairInt
 		ignore       bool
 	}{
 		{
-			input: []string{
-				"p=0,4 v=3,-3",
-				"p=6,3 v=-1,-3",
-				"p=10,3 v=-1,2",
-				"p=2,0 v=2,-1",
-				"p=0,0 v=1,3",
-				"p=3,0 v=-2,-2",
-				"p=7,6 v=-1,-3",
-				"p=3,0 v=-1,-2",
-				"p=9,3 v=2,3",
-				"p=7,3 v=-1,2",
-				"p=2,4 v=2,-3",
-				"p=9,5 v=-3,-3",
-			},
+			input: `p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3
+`,
 			size:   aoc.NewPairInt(11, 7),
 			wantA:  12,
 			wantB:  0,
@@ -40,8 +40,9 @@ func Test_day14(t *testing.T) {
 			continue
 		}
 		t.Run(fmt.Sprintf("Test %v", n), func(t *testing.T) {
-			testPart(t, "A", processA, tc.input, tc.size, tc.wantA)
-			testPart(t, "B", processB, tc.input, tc.size, tc.wantB)
+			puzzle := strings.Split(tc.input, "\n")
+			testPart(t, "A", processA, puzzle, tc.size, tc.wantA)
+			testPart(t, "B", processB, puzzle, tc.size, tc.wantB)
 		})
 	}
 }
